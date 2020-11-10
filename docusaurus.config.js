8e59d82333a535d6edf879c1bfcca1dae13a7b03
Craig McKeachie <craigdmckeachie@gmail.com>
2020-11-10 17:08:05 -0500

    updating links in header and footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,55 +42,66 @@ module.exports = {
           position: 'left',
         },
         // { to: 'blog', label: 'Blog', position: 'left' },
-        // {
-        //   href: 'https://github.com/facebook/docusaurus',
-        //   label: 'GitHub',
-        //   position: 'right',
-        // },
+        {
+          href: 'https://github.com/craigmckeachie/handsonreact.com',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          href:
+            'https://www.youtube.com/channel/UCxJG7GwhiI3lI7pn_HuBLgw?sub_confirmation=1',
+          label: 'YouTube',
+          position: 'right',
+        },
       ],
     },
     footer: {
       style: 'dark',
       links: [
-        // {
-        //   title: 'Docs',
-        //   items: [
-        //     {
-        //       label: 'Docs',
-        //       to: 'docs/',
-        //     },
-        //     {
-        //       label: 'Labs',
-        //       to: 'docs/doc2/',
-        //     },
-        //   ],
-        // },
-        // {
-        //   title: 'Community',
-        //   items: [
-        //     {
-        //       label: 'Twitter',
-        //       href: 'https://twitter.com/docusaurus',
-        //     },
-        //   ],
-        // },
-        // {
-        //   title: 'Links',
-        //   items: [
-        //     // {
-        //     //   label: 'Blog',
-        //     //   to: 'blog',
-        //     // },
-        //     {
-        //       label: 'GitHub',
-        //       href: 'https://github.com/craigmckeachie/handsonreact.com',
-        //     },
-        //   ],
-        // },
+        {
+          title: 'Labs',
+          items: [
+            {
+              label: 'Labs (JavaScript)',
+              to: 'docs/docs/labs/js/01-CreatingNewProject',
+            },
+            {
+              label: 'Labs (TypeScript)',
+              to: 'docs/docs/labs/ts/01-CreatingNewProject',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/cmckeachie',
+            },
+            {
+              label: 'YouTube',
+              href:
+                'https://www.youtube.com/channel/UCxJG7GwhiI3lI7pn_HuBLgw?sub_confirmation=1',
+            },
+          ],
+        },
+        {
+          title: 'Links',
+          items: [
+            // {
+            //   label: 'Blog',
+            //   to: 'blog',
+            // },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/craigmckeachie/handsonreact.com',
+            },
+          ],
+        },
       ],
-      copyright: `This site is licensed under <a href="http://www.gnu.org/licenses/gpl-3.0-standalone.html">
+      copyright: `<br/><br/> This site is licensed under <a href="http://www.gnu.org/licenses/gpl-3.0-standalone.html">
       GPL version 3.
-    </a><br/>Copyright © ${new Date().getFullYear()} Funny Ant, LLC <br/> All rights reserved.`,
+    </a><br/>Copyright © ${new Date().getFullYear()} Funny Ant, LLC. All rights reserved.`,
     },
   },
   presets: [
