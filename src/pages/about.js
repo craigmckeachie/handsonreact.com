889e463ad5feaeff72496ae76470c7af2cc61b89
Craig McKeachie <craigdmckeachie@gmail.com>
2020-11-30 14:27:57 -0500

    about page updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,37 +26,32 @@ function About() {
               <h3>Hi, I’m Craig McKeachie. ​</h3>
               <p>
                 I have been a professional <strong>software developer</strong>{' '}
-                for over <strong>20 years</strong>.
+                for over <strong>20 years</strong>. I like to say that I had a
+                mid-life crisis, but instead of buying a sports car, I quit my
+                job and{' '}
+                <strong>wrote a book about JavaScript frameworks</strong>.
               </p>
+              <p></p>
               <p>
-                I like to say I had a mid-life crisis but instead of buying a
-                sports car I quit my job and <strong>wrote a book</strong>{' '}
-                <em>The JavaScript Framework Guide</em> about the first
-                generation of JavaScript frameworks (which at the time was
-                AngularJS, Backbone, and Ember).
-              </p>
-              <p>
-                After writing the book, I was contacted by several training
-                companies who asked if I wanted to{' '}
-                <strong>try on-site training</strong>. I accepted and really
-                enjoyed teaching, but, at first, I really wasn't that good
-                because I was new to training and the manuals I used to teach
-                were not very good.
-              </p>
-              <p>
-                I worked at it and{' '}
+                <em>The JavaScript Framework Guide </em> was about the first
+                generation of frameworks and included AngularJS, Backbone, and
+                Ember. After writing the book, I was contacted by several
+                training companies who asked if I wanted to{' '}
+                <strong>try on-site training</strong>. I accepted and found that
+                I really enjoyed teaching, but, at first, I wasn't very good. I
+                was new to training and the manuals I used weren't great.
+                However, over time I worked at it ultimately{' '}
                 <strong>
                   became an independent trainer and consultant specializing in
-                  Angular and React
-                </strong>{' '}
-                and <strong>created my own materials </strong>
-                and <em>kept improving them based on student feedback</em>.
+                  React and Angular
+                </strong>
+                .
               </p>
 
               <p>
                 I{' '}
                 <strong>
-                  traveled across the United States helping smart developers
+                  traveled across the United States and helped smart developers
                 </strong>
                 <em> like yourself</em> get up to speed quickly on new
                 JavaScript libraries and frameworks like React and Angular.
@@ -68,8 +63,8 @@ function About() {
                   developers
                 </strong>{' '}
                 on JavaScript frameworks/libraries including <code>React</code>{' '}
-                and <code>Angular</code>
-                <strong>with an average rating of 9.5 out of 10</strong>.
+                and <code>Angular</code>. My{' '}
+                <strong>average rating from students is 9.5 out of 10</strong>.
               </p>
               <p>
                 Now <strong>I want to</strong> use my experience to{' '}
